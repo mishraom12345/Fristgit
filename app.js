4e15398be96e403e29b78319e519d4dcdd01ad08
mishraom12345 <mishrahariom525@gmail.com>
2022-12-15 12:13:21 +0530

    dom manupilation

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("i love git")
+console.dir(document);
+console.log(document.domain)
+console.log(document)
+console.log(document.title)
+document.title = 123
+console.log(document.doctype)
+console.log(document.head)
+console.log(document.body)
+console.log(document.all)
+console.log(document.all[10])
+document.all[10].textContent = "HELLO"
+console.log(document.forms)
+console.log(document.images)
+console.log(document.links)
+
+var hari = document.getElementById("header-title")
+console.log(hari)
+hari.textContent = "PRACHI"
+hari.innerText = "GOLU"
+console.log(hari.textContent)
+console.log(hari.innerText)
+hari.innerHTML = '<h1> sakshi</h1>'
+hari.style.borderBottom = "solid 3px #000"
+
