4a4d2278c5c4f7a0a98f99257a94393818a2e16b
mishraom12345 <mishrahariom525@gmail.com>
2022-12-19 13:48:28 +0530

    task 4.1

--- app.js
+++ app.js
@@ -1,40 +1,15 @@
-console.dir(document);
-console.log(document.domain)
-console.log(document)
-console.log(document.title)
-document.title = 123
-console.log(document.doctype)
-console.log(document.head)
-console.log(document.body)
-console.log(document.all)
-console.log(document.all[10])
-document.all[10].textContent = "HELLO"
-console.log(document.forms)
-console.log(document.images)
-console.log(document.links)
-
-var hari = document.getElementById("header-title")
-console.log(hari)
-hari.textContent = "PRACHI"
-hari.innerText = "GOLU"
-console.log(hari.textContent)
-console.log(hari.innerText)
-hari.innerHTML = '<h1> sakshi</h1>'
-hari.style.borderBottom = "solid 3px #000"
+task 4
+var items = document.getElementsByClassName("list-group-item")
+console.log(items)
+console.log(items[1])
+items[1].textContent = 'HELLO BS KAR'
+items[1].style.fontWeight = 'strong'
+items[1].style.background = 'yellow'
 
 
+items[2].style.background = 'green'
 
+for(i=0;i<items.length; i++){
+    items[i].style.fontWeight = 'bold'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
