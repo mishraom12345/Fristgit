cb012b480fbdb9695e3ed43abab87dea4eb4c19e
mishraom12345 <mishrahariom525@gmail.com>
2022-12-19 22:36:04 +0530

    task 8

--- app.js
+++ app.js
@@ -1,176 +1,74 @@
-// console.dir(document);
-// console.log(document.domain)
-// console.log(document)
-// console.log(document.title)
-// document.title = 123
-// console.log(document.doctype)
-// console.log(document.head)
-// console.log(document.body)
-// console.log(document.all)
-// console.log(document.all[10])
-// document.all[10].textContent = "HELLO"
-// console.log(document.forms)
-// console.log(document.images)
-// console.log(document.links)
-
-// var hari = document.getElementById("header-title")
-// console.log(hari)
-// hari.textContent = "PRACHI"
-// hari.innerText = "GOLU"
-// console.log(hari.textContent)
-// console.log(hari.innerText)
-// hari.innerHTML = '<h1> sakshi</h1>'
-// hari.style.borderBottom = "solid 3px #000"
-
-// task 4
-// var items = document.getElementsByClassName("list-group-item")
-// console.log(items)
-// console.log(items[1])
-// items[1].textContent = 'HELLO BS KAR'
-// items[1].style.fontWeight = 'strong'
-// items[1].style.background = 'yellow'
+// 
 
 
-// items[2].style.background = 'green'
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
-// for(i=0;i<items.length; i++){
-//     items[i].style.fontWeight = 'bold'
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+}
+
+// Remove item
+// Remove item
+// Remove item
+function removeItem(e){
+
+    if(e.target.classList.contains('delete')){
+
+     if(confirm('Are You Sure?')) {
+
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+      }
+    }
+  }
+// // Filter Items
+// function filterItems(e){
+//   // convert text to lowercase
+//   var text = e.target.value.toLowerCase();
+//   // Get lis
+//   var items = itemList.getElementsByTagName('li');
+//   // Convert to an array
+//   Array.from(items).forEach(function(item){
+//     var itemName = item.firstChild.textContent;
+//     if(itemName.toLowerCase().indexOf(text) != -1){
+//       item.style.display = 'block';
+//     } else {
+//       item.style.display = 'none';
+//     }
+//   });
 // }
-
-// task 5
-// var li = document.getElementsByTagName('li')
-// console.log(li)
-// console.log(li[1])
-// li[1].textContent = 'HELLO BS KAR'
-// li[1].style.fontWeight = 'strong'
-// li[1].style.background = 'yellow'
-
-
-// li[2].style.background = 'green'
-
-// for(i=0;i<li.length; i++){
-//    li[i].style.fontWeight = 'bold'
-
-// }
-
-// task 6
-// var prachi = document.querySelector(".list-group-item:nth-child(2)")
-// prachi.style.background = "yellow"
-
-// var sakshi = document.querySelector(".list-group-item:nth-child(3)")
-// sakshi.style.visibility = "hidden"
-
-//  var dev = document.querySelectorAll(".list-group-item")
-//  dev[2].style.color = 'green'
-
-//  var hari = document.querySelectorAll(".list-group-item:nth-child(odd)")
-
-//  for(var i = 0;i<hari.length;i++){
-//     hari[i].style.background = 'green'
-
-//  }
-
-//  var golu = document.querySelectorAll(".list-group-item:nth-child(even)")
-
-//  for(var i = 0;i<hari.length;i++){
-//     golu[i].style.background = 'yellow'
-
-//  }
-
-// // querySelectorAll salect all the element but qureyselector select only the upper most element
-
-
-//task 7
-//var itemList = document.querySelector('#items');
-
-// console.log(itemList.parentNode);
-// itemList.parentNode.style.backgroundcolor = 'blue'
-// console.log(itemList.parentNode.parentNode)
-
-//parent element
-// console.log(itemList.parentElement)
-// itemList.parentElement.style.backgroundColor = "blue"
-// console.log(itemList.parentElement.parentElement)
-
-// console.log(itemList.childNodes)
-// console.log(itemList.children)
-// console.log(itemList.children[2])
-// itemList.children[2].style.backgroundcolor = 'blue'
-
-//last child
-// console.log(itemList.lastChild)
-
-// console.log(itemList.lastElementChild)
-// itemList.lastElementChild.textContent = "HELLO BAS KAR"
-
-// //frist child
-// console.log(itemList.firstChild)
-
-// console.log(itemList.firstElementChild)
-// itemList.firstElementChild.textContent = "chala ja "
-
-//next sibling
-// console.log(itemList.nextSibling)
-
-// console.log(itemList.nextElementSibling)
-
-// //previous sibling
-// console.log(itemList.previousSibling)
-
-// console.log(itemList.previousElementSibling)
-// itemList.previousElementSibling.style.color = 'blue'
-
-//creat a div
-var newDiv = document.createElement('div')
-
-//add class
-newDiv.className = 'hello'
-
-//add id 
-newDiv.id = 'kkrh ho'
-
-//set attribute
-newDiv.setAttribute('tittle','helllo')
-
-//create text node
-var newDivText = document.createTextNode('hello word')
-newDiv.appendChild(newDivText)
-
-var container = document.querySelector('header .container')
-
-var h1 = document.querySelector('header h1')
-
-container.insertBefore(newDiv,h1)
-
-var priti = document.createElement('div')
-priti.appendChild(document.createTextNode('aur batao kaise ho'))
-
-var hari = document.querySelector('ul')
-
-var li = document.querySelector('ul li')
-
-hari.insertBefore(priti,li)
-
-
-
-newDiv.style.fontSize = '40px'
-newDiv.style.color = 'red'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
