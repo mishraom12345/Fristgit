8af822b855fb26a6c662de1adae4269900b19875
mishraom12345 <mishrahariom525@gmail.com>
2022-12-19 13:50:43 +0530

    tak 5

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-task 4
-var items = document.getElementsByClassName("list-group-item")
-console.log(items)
-console.log(items[1])
-items[1].textContent = 'HELLO BS KAR'
-items[1].style.fontWeight = 'strong'
-items[1].style.background = 'yellow'
+var li = document.getElementsByTagName('li')
+console.log(li)
+console.log(li[1])
+li[1].textContent = 'HELLO BS KAR'
+li[1].style.fontWeight = 'strong'
+li[1].style.background = 'yellow'
 
 
-items[2].style.background = 'green'
+li[2].style.background = 'green'
 
-for(i=0;i<items.length; i++){
-    items[i].style.fontWeight = 'bold'
+for(i=0;i<li.length; i++){
+   li[i].style.fontWeight = 'bold'
 
 }
+
